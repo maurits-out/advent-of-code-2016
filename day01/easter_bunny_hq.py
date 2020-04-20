44c24cd3ede12b04426491706566249345e300a3
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-04-20 20:38:15 +0200

    Refactoring. Solved warnings.

--- day01/easter_bunny_hq.py
+++ day01/easter_bunny_hq.py
@@ -1,10 +1,12 @@
 from enum import Enum, auto
 
+
 class Heading(Enum):
     NORTH = auto()
     EAST = auto()
     SOUTH = auto()
     WEST = auto()
+
 
 def turn_left(heading):
     if heading == Heading.NORTH:
@@ -15,25 +17,30 @@ def turn_left(heading):
         return Heading.EAST
     return Heading.NORTH
 
+
 def turn_right(heading):
     if heading == Heading.NORTH:
         return Heading.EAST
     if heading == Heading.EAST:
         return Heading.SOUTH
-    if heading ==Heading.SOUTH:
+    if heading == Heading.SOUTH:
         return Heading.WEST
     return Heading.NORTH
+
 
 def turn(heading, direction):
     if direction == "R":
         return turn_right(heading)
     return turn_left(heading)
 
+
 def get_direction(instruction):
     return instruction[0:1]
 
+
 def get_number_of_blocks(instruction):
     return int(instruction[1:])
+
 
 def get_delta_row(heading):
     if heading == Heading.NORTH:
@@ -42,12 +49,14 @@ def get_delta_row(heading):
         return 1
     return 0
 
+
 def get_delta_column(heading):
     if heading == Heading.WEST:
         return -1
     if heading == Heading.EAST:
         return 1
     return 0
+
 
 def move_blocks(current_row, current_column, heading, number_of_blocks):
     delta_row = get_delta_row(heading)
@@ -59,8 +68,9 @@ def move_blocks(current_row, current_column, heading, number_of_blocks):
         locations.append((current_row, current_column))
     return locations
 
+
 def follow_instructions(instructions):
-    locations = [(0,0)]
+    locations = [(0, 0)]
     heading = Heading.NORTH
     for instruction in instructions:
         heading = turn(heading, get_direction(instruction))
@@ -69,11 +79,14 @@ def follow_instructions(instructions):
         locations = locations + move_blocks(*current_location, heading, number_of_blocks)
     return locations
 
+
 def distance(row, column):
     return abs(row) + abs(column)
 
+
 def find_location_for_part1(locations):
     return locations[-1]
+
 
 def find_location_for_part2(locations):
     visited = set()
@@ -83,11 +96,16 @@ def find_location_for_part2(locations):
         visited.add(location)
     return None
 
-input = "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2"
-locations = follow_instructions(input.split(", "))
 
-distance_part1 = distance(*find_location_for_part1(locations))
-print("Easter Bunny HQ is {} blocks away (part 1).".format(distance_part1))
+def main():
+    with open("input.txt", "r") as file:
+        instructions = file.read()
+    locations = follow_instructions(instructions.split(", "))
+    distance_part1 = distance(*find_location_for_part1(locations))
+    print("Easter Bunny HQ is {} blocks away (part 1).".format(distance_part1))
+    distance_part2 = distance(*find_location_for_part2(locations))
+    print("Easter Bunny HQ is {} blocks away (part 2).".format(distance_part2))
 
-distance_part2 = distance(*find_location_for_part2(locations))
-print("Easter Bunny HQ is {} blocks away (part 2).".format(distance_part2))
+
+if __name__ == '__main__':
+    main()
--- day02/bathroom_security.py
+++ day02/bathroom_security.py
@@ -3,37 +3,55 @@ def bathroom_code(keypad, instructions, initial_location):
     code = ""
     for instruction in instructions:
         for char in instruction:
-            next_location = get_next_location(char, current_location)
-            if is_valid(keypad, next_location):
+            next_location = get_next_location(char, *current_location)
+            if is_valid(keypad, *next_location):
                 current_location = next_location
         code = code + keypad[current_location[0]][current_location[1]]
     return code
 
-def get_next_location(char, location):
-    if char == "U":
-        return (location[0] - 1, location[1])
-    if char == "L":
-        return (location[0], location[1] - 1)
-    if char == "R":
-        return (location[0], location[1] + 1)
-    return (location[0] + 1, location[1])
 
-def is_valid(keypad, location):
-    return location[0] in range(len(keypad)) and location[1] in range(len(keypad[location[0]])) and keypad[location[0]][location[1]] != " "
+def get_next_location(char, row, column):
+    if char == "U":
+        return row - 1, column
+    if char == "L":
+        return row, column - 1
+    if char == "R":
+        return row, column + 1
+    return row + 1, column
+
+
+def is_valid(keypad, row, column):
+    return is_in_keypad(keypad, row, column) and is_button(keypad, row, column)
+
+
+def is_button(keypad, row, column):
+    return keypad[row][column] != " "
+
+
+def is_in_keypad(keypad, row, column):
+    return row in range(len(keypad)) and column in range(len(keypad[row]))
+
 
 def part1(instructions):
     keypad = ["123", "456", "789"]
     initial_location = (1, 1)
     return bathroom_code(keypad, instructions, initial_location)
 
+
 def part2(instructions):
     keypad = ["  1  ", " 234 ", "56789", " ABC ", "  D  "]
     initial_location = (2, 0)
     return bathroom_code(keypad, instructions, initial_location)
 
-instructions = []
-with open("day02/input.txt", "r") as file:
-    for line in file:
-        instructions.append(line.strip())
-print("The bathroom code for part 1 is: {}".format(part1(instructions)))
-print("The bathroom code for part 2 is: {}".format(part2(instructions)))
+
+def main():
+    instructions = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            instructions.append(line.strip())
+    print("The bathroom code for part 1 is: {}".format(part1(instructions)))
+    print("The bathroom code for part 2 is: {}".format(part2(instructions)))
+
+
+if __name__ == '__main__':
+    main()
