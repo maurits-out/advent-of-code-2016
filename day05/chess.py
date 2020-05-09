cfcd5e1b1ddcabb3525e16c3670f620d30278bd7
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-05-09 21:17:58 +0200

    Day 5.

---
+++ day05/chess.py
@@ -0,0 +1,71 @@
+import hashlib
+
+PUZZLE_INPUT = "uqwqemis"
+PASSWORD_LENGTH = 8
+HASH_PREFIX = "00000"
+PLACEHOLDER = "_"
+
+
+def apply_md5_hash(puzzle_input, index):
+    value_to_hash = puzzle_input + str(index)
+    md5 = hashlib.md5(value_to_hash.encode())
+    return md5.digest().hex()
+
+
+def is_valid_hash(md5_hash):
+    return md5_hash.startswith(HASH_PREFIX)
+
+
+def is_password_incomplete(password_chars):
+    for char in password_chars:
+        if char == PLACEHOLDER:
+            return True
+    return False
+
+
+def is_invalid_position_char(char):
+    return not "0" <= char < "8"
+
+
+def is_position_taken(password_chars, pos):
+    return password_chars[pos] != PLACEHOLDER
+
+
+def join_password_chars(password_chars):
+    return "".join(password_chars)
+
+
+def find_password_part1(puzzle_input):
+    index = -1
+    password_chars = []
+    while len(password_chars) < PASSWORD_LENGTH:
+        index += 1
+        md5_hash = apply_md5_hash(puzzle_input, index)
+        if is_valid_hash(md5_hash):
+            password_chars.append(md5_hash[5])
+    return join_password_chars(password_chars)
+
+
+def find_password_part2(puzzle_input):
+    index = -1
+    password_chars = list(PLACEHOLDER * 8)
+    while is_password_incomplete(password_chars):
+        index += 1
+        md5_hash = apply_md5_hash(puzzle_input, index)
+        if is_valid_hash(md5_hash):
+            if is_invalid_position_char(md5_hash[5]):
+                continue
+            pos = int(md5_hash[5])
+            if is_position_taken(password_chars, pos):
+                continue
+            password_chars[pos] = md5_hash[6]
+    return join_password_chars(password_chars)
+
+
+def main():
+    print("The password for part 1 is {}".format(find_password_part1(PUZZLE_INPUT)))
+    print("The password for part 2 is {}".format(find_password_part2(PUZZLE_INPUT)))
+
+
+if __name__ == '__main__':
+    main()
