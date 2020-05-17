a93e47fc8b0e2a26e932fdbe0d7042e66ac68206
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-05-17 19:51:04 +0200

    Refactoring

--- day09/explosives.py
+++ day09/explosives.py
@@ -4,20 +4,20 @@ def marker_start(char):
 
 def decompressed_length_part1(contents):
     idx, length, value, subsequence_length = 0, 0, 0, 0
-    parsing_number = False
+    parsing_marker = False
     while idx < len(contents):
-        if parsing_number and "0" <= contents[idx] <= "9":
+        if parsing_marker and "0" <= contents[idx] <= "9":
             value = value * 10 + int(contents[idx])
-        elif parsing_number and contents[idx] == "x":
+        elif parsing_marker and contents[idx] == "x":
             subsequence_length = value
             value = 0
-        elif parsing_number and contents[idx] == ")":
+        elif parsing_marker and contents[idx] == ")":
             length += subsequence_length * value
             idx += subsequence_length
-            parsing_number = False
+            parsing_marker = False
         elif contents[idx] == "(":
             value = 0
-            parsing_number = True
+            parsing_marker = True
         else:
             length += 1
         idx += 1
